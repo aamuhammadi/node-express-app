52fbfd925bb44409de0c3dd3b5464d7a72b19951
aamuhammadi <aamconsultants17@gmail.com>
2023-10-30 21:51:29 +0500

    class 68

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const appRoutes = require("./src/routes/app.routes");
 
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => {
-  res.send("asslamu alaikum");
+app.use("/api", appRoutes);
+
+app.use((req, res) => {
+  res.status(404).send(`<h1>404 - Page Not Found</h1>`);
 });
 
 app.listen(port, () => {
