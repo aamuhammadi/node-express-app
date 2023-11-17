173d411eefedd5aee828468062244e534dfca099
aamuhammadi <aamconsultants17@gmail.com>
2023-11-17 18:49:08 +0500

    class 78

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -91,3 +91,36 @@ exports.getUserInfo = async (req, res) => {
     res.status(500).json({ message: "Something went wrong" });
   }
 };
+
+exports.updateUser = async (req, res) => {
+  const userId = req.user._id;
+  const { firstName, lastName, email, password } = req.body;
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.firstName = firstName || user.firstName;
+    user.lastName = lastName || user.lastName;
+    user.email = email || user.email;
+
+    if (password) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      user.password = hashedPassword;
+    }
+
+    const updatedUser = await user.save();
+
+    const { password: userPassword, ...userWithoutPassword } = updatedUser._doc;
+
+    res.status(200).json({
+      message: "User updated successfully",
+      user: userWithoutPassword,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.get("/info", protect, userController.getUserInfo);
+router.put("/update", protect, userController.updateUser);
 
 module.exports = router;
